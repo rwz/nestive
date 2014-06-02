cdca78d9452d72f91d46e837db7259411a6f6651
Pavel Pravosud <pavel@pravosud.com>
2014-06-02 13:55:55 -0400

    Upgrade to RSpec 3

--- Gemfile
+++ Gemfile
@@ -5,5 +5,5 @@ gemspec
 
 group :test do
   gem 'combustion', '~> 0.5.1'
-  gem 'rspec-rails', '~> 2.13'
+  gem 'rspec-rails', '~> 3.0'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,7 @@ require 'nestive'
 Combustion.initialize! :action_controller
 
 require 'rspec/rails'
+
+RSpec.configure do |config|
+  config.infer_spec_type_from_file_location!
+end
