1cc357ab94a8f2b3e99424fb37dd2a2873ef92c1
expeehaa <expeehaa@outlook.com>
2021-05-28 01:15:09 +0200

    Fix deprecation warning of dynamic :controller and :action segments in the route definition.

--- spec/internal/config/routes.rb
+++ spec/internal/config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  get ':controller/:action'
+  %w[
+    index
+    append
+    prepend
+    replace
+    purge_single
+    purge_multiple
+    extended_one
+    extended_two
+    extended_three
+    extended_without_yield
+  ].each do |action|
+    get "nestive/#{action}", to: "nestive##{action}"
+  end
 end
